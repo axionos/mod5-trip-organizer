fd0a28963fcb7672f923c10379003346b9311e1d
axionos <axionoso@gmail.com>
2019-06-26 13:19:11 -0400

    fetch trips data

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -2,7 +2,9 @@ import React from 'react';
 // import { Link } from 'react-router-dom'
 
 class Trip extends React.Component {
+
   render(){
+    console.log('Trip Props', this.props)
     return(
       <div>
         Trip Component
