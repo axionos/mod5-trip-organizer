d130c0ab9c9fea321c57336dae14265ae3c8674f
axionos <axionoso@gmail.com>
2019-06-28 17:17:27 -0400

    calculate number of days between two dates

--- src/containers/ItineraryList.js
+++ src/containers/ItineraryList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Moment from 'moment'
 import { connect } from 'react-redux'
 
 class ItineraryList extends React.Component {
   render(){
     console.log('Itinerary List Props', this.props)
+
+    // CALCULATE HOW MANY DAYS THERE ARE IN BETWEEN TWO DATES
+    const startDate = Moment(this.props.theTrip.startDate, "YYYY-MM-DD")
+    const endDate = Moment(this.props.theTrip.endDate, "YYYY-MM-DD")
+    const answer = endDate.diff(startDate, 'days')
+    console.log(answer)
+    // END CALCULATING
+
     return(
       <div>
         <h2>{this.props.theTrip.title}</h2>
