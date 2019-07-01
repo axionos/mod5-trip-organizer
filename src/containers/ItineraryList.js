4292c668028fb7f86ac5bc974ce4426496967103
axionos <axionoso@gmail.com>
2019-07-01 15:46:47 -0400

    fetch days data

--- src/containers/ItineraryList.js
+++ src/containers/ItineraryList.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import Moment from 'moment'
+// import Moment from 'moment'
 import { connect } from 'react-redux'
 import { Container } from 'semantic-ui-react'
 
 class ItineraryList extends React.Component {
+
+  componentDidMount(){
+    const id = this.props.theTrip.id
+    fetch(`http://localhost:3000/days/${id}`, {
+      headers: {
+        'Authorization': localStorage.getItem("token")
+      }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+
   render(){
     console.log('Itinerary List Props', this.props)
-
-    // // CALCULATE HOW MANY DAYS THERE ARE IN BETWEEN TWO DATES
-    // const startDate = Moment(this.props.theTrip.startDate, "YYYY-MM-DD")
-    // const endDate = Moment(this.props.theTrip.endDate, "YYYY-MM-DD")
-    // const answer = endDate.diff(startDate, 'days')
-    // console.log(answer)
-    // // END CALCULATING
 
     return(
       <Container className='page-container'>
@@ -25,8 +31,23 @@ class ItineraryList extends React.Component {
   }
 }
 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     getTrip: trip => {
+//       dispatch(getTrip(trip))
+//     }
+//   }
+// }
+
 const mapStateToProps = (state) => {
   return { theTrip: state.theTrip[0] }
 }
 
 export default connect(mapStateToProps)(ItineraryList)
+
+// // CALCULATE HOW MANY DAYS THERE ARE IN BETWEEN TWO DATES
+// const startDate = Moment(this.props.theTrip.startDate, "YYYY-MM-DD")
+// const endDate = Moment(this.props.theTrip.endDate, "YYYY-MM-DD")
+// const answer = endDate.diff(startDate, 'days')
+// console.log(answer)
+// // END CALCULATING
