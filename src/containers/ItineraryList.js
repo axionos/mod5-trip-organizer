2465fc018174ae6dfb13c3cae27e454b7618d680
axionos <axionoso@gmail.com>
2019-07-01 17:56:04 -0400

    fixing the side tab on Itinerary List

--- src/containers/ItineraryList.js
+++ src/containers/ItineraryList.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Day from '../components/Day'
 // import Moment from 'moment'
 import { connect } from 'react-redux'
-import { Container } from 'semantic-ui-react'
+import { Container, Grid, Menu, Segment } from 'semantic-ui-react'
 import { getDays } from '../actions/index.js'
 
 class ItineraryList extends React.Component {
+
+  state = { activeItem: 'bio' }
 
   componentDidMount(){
     const id = this.props.theTrip.id
@@ -28,7 +30,7 @@ class ItineraryList extends React.Component {
 
   render(){
     // console.log('Itinerary List Props', this.props)
-
+    const { activeItem } = this.state
     return(
       <Container className='page-container'>
         <h2>{this.props.theTrip.title}</h2>
@@ -36,8 +38,36 @@ class ItineraryList extends React.Component {
         <p>{this.props.theTrip.endDate}</p>
         <p>{this.props.theTrip.destination}</p>
         <div>
+
           { this.genDays() }
         </div>
+
+        <Grid>
+          <Grid.Column width={4}>
+            <Menu fluid vertical tabular>
+              <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
+              <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
+              <Menu.Item
+                name='companies'
+                active={activeItem === 'companies'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                name='links'
+                active={activeItem === 'links'}
+                onClick={this.handleItemClick}
+              />
+            </Menu>
+          </Grid.Column>
+
+          <Grid.Column stretched width={12}>
+            <Segment>
+              This is an stretched grid column. This segment will always match the tab height
+            </Segment>
+          </Grid.Column>
+        </Grid>
+
+
       </Container>
     )
   }
