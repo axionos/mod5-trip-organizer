bbd784ca88f633484963034e76ede993b9788723
axionos <axionoso@gmail.com>
2019-07-03 12:09:19 -0400

    implement react google map without markers

---
+++ src/components/DoctorMarker.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Marker } from "react-google-maps";
+import WatermelonIcon from "../img/watermelon.png";
+
+export default class DoctorMarker extends React.Component {
+
+  render(){
+    return(
+        <Marker
+          position={this.props.location}
+          icon={WatermelonIcon}
+        >
+        </Marker>
+    );
+  }
+}
---
+++ src/components/DoctorsMapContainer.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import DoctorsMap from "../components/DoctorsMap";
+
+export default class DoctorsMapContainer extends React.Component {
+
+	render() {
+    console.log('DoctorsMap Container Props', this.props)
+		return (
+			<DoctorsMap
+				doctors={this.props.doctors}
+				googleMapURL={`https://maps.googleapis.com/maps/api/js?***REMOVED***=AIzaSyBaGD-h-zdNd5SLcDto3jevpeaHXCNRpz4&v=3.exp&libraries=geometry,drawing,places`}
+				loadingElement={<div style={{ height: `100%` }} />}
+				containerElement={<div style={{ height: `600px`, width: `600px` }} />}
+				mapElement={<div style={{ height: `100%` }} />}
+			/>
+		);
+	}
+}
