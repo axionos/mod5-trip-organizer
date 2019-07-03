08a1aa915f328d64f1e1a0c5a8345e2f7913cdb3
axionos <axionoso@gmail.com>
2019-07-03 18:04:39 -0400

    render marker working

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -5,16 +5,16 @@ import Marker from "./Marker";
 const Map = withScriptjs(withGoogleMap((props) =>{
 
   const markers = props.items.map( item => <Marker
-                    key={item.uid}
+                    key={item.id}
                     item={item}
-                    location={{lat:item.latitude, lng: item.longitude}}
+                    location={{lat:parseFloat(item.latitude), lng: parseFloat(item.longitude)}}
                     // location={{lat: item.closestPractice.lat, lng: item.closestPractice.lon}}
                   />);
   console.log('Map Props', props);
   return (
       <GoogleMap
-        defaultZoom={14}
-        center={ { lat:  42.3601, lng: -71.0589 } }
+        defaultZoom={12}
+        center={ { lat:  40.7127753, lng: -74.0059728 } }
       >
         { markers }
       </GoogleMap>
