e9136c53d68eb87827c2c24ad47a9ac99d238df6
axionos <axionoso@gmail.com>
2019-07-04 12:17:43 -0400

    change layout of trip itinerary with map and contents

--- src/components/map/MapContainer.js
+++ src/components/map/MapContainer.js
@@ -11,7 +11,7 @@ export default class MapContainer extends React.Component {
 				items={this.props.items}
 				googleMapURL={`https://maps.googleapis.com/maps/api/js?***REMOVED***=AIzaSyBaGD-h-zdNd5SLcDto3jevpeaHXCNRpz4&v=3.exp&libraries=geometry,drawing,places`}
 				loadingElement={<div style={{ height: `100%` }} />}
-				containerElement={<div style={{ height: `600px`, width: `600px` }} />}
+				containerElement={<div style={{ height: `400px`, width: `100%`, border: `1px solid #aaa` }} />}
 				mapElement={<div style={{ height: `100%` }} />}
 			/>
 		);
