dc6045356ac95d89510bef1523c1853e3148968c
axionos <axionoso@gmail.com>
2019-07-07 12:44:13 -0400

    fix nested item data issue

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -5,12 +5,24 @@ import Marker from "./Marker";
 
 const Map = withScriptjs(withGoogleMap((props) =>{
 
-  const markers = props.items.map( item => <Marker
-                    ***REMOVED***={item.id}
-                    item={item}
-                    location={{lat:parseFloat(item.latitude), lng: parseFloat(item.longitude)}}
-                  />);
-  // console.log('Map Props', props);
+  const markers = props.items.map( item => {
+    // return item.map(theItem => {
+    //   return <Marker
+    //           ***REMOVED***={theItem.id}
+    //           item={theItem}
+    //           location={{lat:parseFloat(theItem.latitude), lng: parseFloat(theItem.longitude)}}
+    //         />
+    // })
+
+      return <Marker
+              ***REMOVED***={item.id}
+              item={item}
+              location={{lat:parseFloat(item.latitude), lng: parseFloat(item.longitude)}}
+            />
+
+  });
+
+  console.log('Map Props', props);
   // debugger
   return (
       <GoogleMap
