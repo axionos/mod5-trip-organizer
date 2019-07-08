4656ecf44213f13202b4e07d427339aaa2f8467e
axionos <axionoso@gmail.com>
2019-07-08 12:00:52 -0400

    renders center location dynamically

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -6,31 +6,30 @@ import Marker from "./Marker";
 const Map = withScriptjs(withGoogleMap((props) =>{
 
   const markers = props.items.map( item => {
-    // return item.map(theItem => {
-    //   return <Marker
-    //           key={theItem.id}
-    //           item={theItem}
-    //           location={{lat:parseFloat(theItem.latitude), lng: parseFloat(theItem.longitude)}}
-    //         />
-    // })
-
-      return <Marker
-              key={item.id}
-              item={item}
-              location={{lat:parseFloat(item.latitude), lng: parseFloat(item.longitude)}}
-            />
-
+    return <Marker
+            key={item.id}
+            item={item}
+            location={{lat:parseFloat(item.latitude), lng: parseFloat(item.longitude)}}
+          />
   });
 
-  console.log('Map Props', props);
-  // debugger
+  console.log('Map Props', props.items);
   return (
+      <React.Fragment>
+
+      {props.items.length === 0 ? null :
+
       <GoogleMap
         defaultZoom={12}
-        center={ { lat:  40.7127753, lng: -74.0059728 } }
+        center={{ lat:  parseFloat(props.items[0].latitude), lng: parseFloat(props.items[0].longitude) }}
       >
         { markers }
       </GoogleMap>
+
+
+
+    }
+</React.Fragment>
     );
   }
 ))
